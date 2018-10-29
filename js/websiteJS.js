a8ab7bba2d04fb54faf52b3f81908e4d02542f07
uvanchang <evanmchang@gmail.com>
2018-10-29 02:08:01 -0700

    remove jumbotron js. updated resume.

--- js/websiteJS.js
+++ js/websiteJS.js
@@ -5,15 +5,3 @@ function mouseOver() {
 function mouseAway() {
   document.getElementById("me").src = "../img/mePop.png";
 }
-
-function jumboOver() {
-  document.getElementById("homeJumbo").style.backgroundColor = "rgba(200, 200, 200, 0)";
-  document.getElementById("title1").style.color = "white";
-  document.getElementById("title2").style.color = "white";
-}
-
-function jumboAway() {
-  document.getElementById("homeJumbo").style.backgroundColor = "rgba(200, 200, 200, 0.7)";
-  document.getElementById("title1").style.color = "black";
-  document.getElementById("title2").style.color = "black";
-}
